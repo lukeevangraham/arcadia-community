d999a74074cee3739290d2feab0f0d8e78fe312a
Luke Graham <luke.evan.graham@gmail.com>
2022-06-11 17:06:12 -0700

    curve up top

--- components/Home/Header/Header.js
+++ components/Home/Header/Header.js
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import Toolbar from "../../Navigation/Toolbar/Toolbar";
 
-import styles from "./Header.module.scss";
+import classes from "./Header.module.scss";
 
 const Header = ({ children, homeHeaderImage }) => (
-  <div className={styles.Header}>
+  <div className={classes.Header}>
     {children}
     <Image
       src={homeHeaderImage}
       layout="fill"
       objectFit="cover"
-      className={styles.HeaderImage}
+      className={classes.HeaderImage}
     ></Image>
-    <div className={styles.HeaderText}>Welcome To Arcadia Community Church</div>
-    <div className={styles.Overlay}></div>
+    <div className={classes.HeaderText}>Welcome To Arcadia Community Church</div>
+    <div className={classes.Overlay}></div>
     {console.log("HERE: ", homeHeaderImage)}
+    <div className={classes.Arc}></div>
   </div>
 );
 
--- components/Navigation/Toolbar/Toolbar.js
+++ components/Navigation/Toolbar/Toolbar.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import Link from "next/link"
+import Link from "next/link";
 import styles from "./Toolbar.module.scss";
 
 const Toolbar = ({ globalData }) => (
-  <div className={styles.Toolbar}>
+  <div className={`${styles.Toolbar}`}>
     {console.log("GLO: ", globalData)}
     <div className={styles.Logo}>
       <Image
@@ -12,8 +12,12 @@ const Toolbar = ({ globalData }) => (
       />
     </div>
     <ul className={styles.Links}>
-      {globalData.data.attributes.Navbar.links.map(link => (
-        <li key={link.id}><Link href={link.url}><a>{link.text}</a></Link></li>
+      {globalData.data.attributes.Navbar.links.map((link) => (
+        <li key={link.id}>
+          <Link href={link.url}>
+            <a>{link.text}</a>
+          </Link>
+        </li>
       ))}
     </ul>
   </div>
