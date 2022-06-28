f96117c7571af1c9414d1f151ff9b045b880ad11
Luke Graham <luke.evan.graham@gmail.com>
2022-06-28 15:41:06 -0700

    feature row can respond with only bg picture

--- components/Sections/FeatureRowsGroup/FeatureRowsGroup.js
+++ components/Sections/FeatureRowsGroup/FeatureRowsGroup.js
@@ -8,30 +8,36 @@ const FeatureRowsGroup = ({ data }) => (
       <div key={row.id} className={classes.FeatureRow}>
         {console.log("ROW: ", row)}
         <div className={classes.FeatureRow__pictures}>
-          <div className={classes.FeatureRow__pictures_bg}>
-            <Image
-              src={row.bgImage.data.attributes.url}
-              alt={row.bgImage.data.attributes.alternativeText}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <div className={`${classes.FeatureRow__pictures_1}`}>
-            <Image
-              src={row.middleImage.data.attributes.url}
-              alt={row.middleImage.data.attributes.alternativeText}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <div className={classes.FeatureRow__pictures_2}>
-            <Image
-              src={row.topImage.data.attributes.url}
-              alt={row.topImage.data.attributes.alternativeText}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
+          {row.bgImage ? (
+            <div className={classes.FeatureRow__pictures_bg}>
+              <Image
+                src={row.bgImage.data.attributes.url}
+                alt={row.bgImage.data.attributes.alternativeText}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          ) : null}
+          {row.middleImage.data ? (
+            <div className={`${classes.FeatureRow__pictures_1}`}>
+              <Image
+                src={row.middleImage.data.attributes.url}
+                alt={row.middleImage.data.attributes.alternativeText}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          ) : null}
+          {row.middleImage.data ? (
+            <div className={classes.FeatureRow__pictures_2}>
+              <Image
+                src={row.topImage.data.attributes.url}
+                alt={row.topImage.data.attributes.alternativeText}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          ) : null}
         </div>
         <div className={classes.FeatureRow__Text}>
           <h4>{row.smallHeading}</h4>
