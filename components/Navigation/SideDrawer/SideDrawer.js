afe20d25bf492ad174707ea371f16d1475137b6a
Luke Graham <luke.evan.graham@gmail.com>
2022-07-09 14:08:19 -0700

    working on logo link in sideDrawer

--- components/Navigation/SideDrawer/SideDrawer.js
+++ components/Navigation/SideDrawer/SideDrawer.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import Backdrop from "../../UI/Backdrop/Backdrop";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import Brandname from "../../UI/Brandname/Brandname";
@@ -15,19 +16,23 @@ const SideDrawer = ({ open, closed, globalData }) => (
           : `${classes.SideDrawer} ${classes.SideDrawer__closed}`
       }
     >
-      <div className={classes.SideDrawer__logo}>
-        <Image
-          src={globalData.data.attributes.Navbar.logo.data.attributes.url}
-          alt={
-            globalData.data.attributes.Navbar.logo.data.attributes
-              .alternativeText
-          }
-          layout="fill"
-        />
-      </div>
-      <div className={classes.SideDrawer__name}>
-        <Brandname />
-      </div>
+      <Link href="/">
+        <a style={{ display: "contents" }}>
+          <div className={classes.SideDrawer__logo}>
+            <Image
+              src={globalData.data.attributes.Navbar.logo.data.attributes.url}
+              alt={
+                globalData.data.attributes.Navbar.logo.data.attributes
+                  .alternativeText
+              }
+              layout="fill"
+            />
+          </div>
+          <div className={classes.SideDrawer__name}>
+            <Brandname />
+          </div>
+        </a>
+      </Link>
       <NavigationItems links={globalData.data.attributes.Navbar.links} />
       {/* {globalData.data.attributes.Navbar.links.map((navLink) => (
         <div>{navLink.text}</div>
