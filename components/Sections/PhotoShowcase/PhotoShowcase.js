2023fec2428b2fe42e19db6038eb47dcf739945e
Luke Graham <luke.evan.graham@gmail.com>
2022-07-13 13:17:17 -0700

    auto calculate photoshowcase photo width

--- components/Sections/PhotoShowcase/PhotoShowcase.js
+++ components/Sections/PhotoShowcase/PhotoShowcase.js
@@ -5,16 +5,16 @@ import classes from "./PhotoShowcase.module.scss";
 const PhotoShowcase = ({ data }) => (
   <section className={classes.PhotoShowcase}>
     <ul className={classes.PhotoShowcase__Group}>
-      {data.photos.data.map((photo) => (
-        <li key={photo.id}>
+      {data.photos.data.map((photo, index, array) => (
+        // calculate how wide the pictures should be
+        <li key={photo.id} style={{ width: `${100 / (array.length / 2)}%` }}>
+          {console.log("ARR: ", array.length / 2)}
           <figure className={classes.PhotoShowcase__Group_Photo}>
             <Image
               src={photo.attributes.url}
               alt={photo.attributes.alternativeText}
               layout="fill"
               objectFit="cover"
-              // width={800}
-              // height={600}
             />
           </figure>
         </li>
