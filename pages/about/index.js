c49f0374192d7d5c5e10ebef7eadffebc11954d0
Luke Graham <luke.evan.graham@gmail.com>
2022-07-19 23:20:59 -0700

    allowFullScreen

--- pages/about/index.js
+++ pages/about/index.js
@@ -126,7 +126,7 @@ const About = ({ globalData, staffData, aboutData }) => (
           width="100%"
           height="450"
           style={{ border: 0 }}
-          allowfullscreen=""
+          allowFullScreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
