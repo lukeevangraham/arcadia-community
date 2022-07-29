0eb6e480d5f2a7b7b56ebd884bca3648f8dca8c7
Luke Graham <luke.evan.graham@gmail.com>
2022-07-29 10:25:56 -0700

    ministry page redirects to external sites when requested

--- components/Ministries/MinistryCard.js
+++ components/Ministries/MinistryCard.js
@@ -4,33 +4,42 @@ import DefaultBgImage from "../UI/DefaultBgImage/DefaultBgImage";
 
 import classes from "./MinistryCard.module.scss";
 
-const MinistryCard = ({ ministry, globalData }) => (
-  <div key={ministry.id}>
-    <Link href={`/ministry/${ministry.attributes.Slug}`}>
-      <a>
-        <div className={classes.MinistryCard}>
-          <div className={classes.MinistryCard_Image}>
-            {ministry.attributes.primaryPhoto.data ? (
-              <Image
-                src={ministry.attributes.primaryPhoto.data.attributes.url}
-                alt={
-                  ministry.attributes.primaryPhoto.data.attributes.alternateText
-                }
-                layout="fill"
-                objectFit="cover"
-              />
-            ) : (
-              <DefaultBgImage globalData={globalData} />
-            )}
-          </div>
-          <div className={classes.MinistryCard_Color}></div>
-          <h3 className={classes.MinistryCard_Name}>
-            {ministry.attributes.ministryName}
-          </h3>
-        </div>
-      </a>
-    </Link>
-  </div>
-);
+const MinistryCard = ({ ministry, globalData }) => {
+  const renderMinCardInterior = (
+    <div className={classes.MinistryCard}>
+      <div className={classes.MinistryCard_Image}>
+        {ministry.attributes.primaryPhoto.data ? (
+          <Image
+            src={ministry.attributes.primaryPhoto.data.attributes.url}
+            alt={ministry.attributes.primaryPhoto.data.attributes.alternateText}
+            layout="fill"
+            objectFit="cover"
+          />
+        ) : (
+          <DefaultBgImage globalData={globalData} />
+        )}
+      </div>
+      <div className={classes.MinistryCard_Color}></div>
+      <h3 className={classes.MinistryCard_Name}>
+        {ministry.attributes.ministryName}
+      </h3>
+    </div>
+  );
+
+  return (
+    <div key={ministry.id} className={classes.MinistryCard}>
+      {ministry.attributes.RedirectToAnotherSite ? (
+        <a href={ministry.attributes.RedirectToAnotherSite} target="_blank">
+          {renderMinCardInterior}
+        </a>
+      ) : (
+        <Link href={`/ministry/${ministry.attributes.Slug}`}>
+          <a>{renderMinCardInterior}</a>
+        </Link>
+        // <div>Fail</div>
+      )}
+    </div>
+  );
+};
 
 export default MinistryCard;
