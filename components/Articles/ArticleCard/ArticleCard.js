7d6f33909d3125282d1c92ef2084f95928dbeb3a
Luke Graham <luke.evan.graham@gmail.com>
2022-07-31 12:24:23 -0700

    articleCard has scale transform on hover

--- components/Articles/ArticleCard/ArticleCard.js
+++ components/Articles/ArticleCard/ArticleCard.js
@@ -8,58 +8,60 @@ import classes from "./ArticleCard.module.scss";
 const dateOptions = { month: "short", day: "numeric", year: "numeric" };
 
 const ArticleCard = ({ article, globalData, style }) => (
-  <div style={style} className={classes.Article} key={article.id}>
-    <Link href={`/news/${article.attributes.slug}`}>
-      <a>
-        <div className={classes.Article_image}>
-          {article.attributes.image.data ? (
-            <Image
-              src={article.attributes.image.data.attributes.url}
-              alt={article.attributes.image.data.attributes.alternativeText}
-              layout="fill"
-              objectFit="cover"
-            />
-          ) : (
-            <DefaultBgImage globalData={globalData} />
-          )}
-        </div>
-      </a>
-    </Link>
-    <div className={classes.Article__NotImage}>
+  <div style={{ ...style, display: "flex", width: "100%" }}>
+    <div className={classes.Article}>
       <Link href={`/news/${article.attributes.slug}`}>
         <a>
-          <div className={classes.Article__NotImage_title}>
-            {article.attributes.title}
+          <div className={classes.Article_image}>
+            {article.attributes.image.data ? (
+              <Image
+                src={article.attributes.image.data.attributes.url}
+                alt={article.attributes.image.data.attributes.alternativeText}
+                layout="fill"
+                objectFit="cover"
+              />
+            ) : (
+              <DefaultBgImage globalData={globalData} />
+            )}
           </div>
         </a>
       </Link>
-      <div className={classes.Article__NotImage_belowTitle}>
-        <div>
-          <svg>
-            <use xlinkHref="/images/sprite.svg#icon-user"></use>
-          </svg>
-          <div>{article.attributes.author}</div>
-        </div>
-        <div>
-          <svg>
-            <use xlinkHref="/images/sprite.svg#icon-calendar"></use>
-          </svg>
+      <div className={classes.Article__NotImage}>
+        <Link href={`/news/${article.attributes.slug}`}>
+          <a>
+            <div className={classes.Article__NotImage_title}>
+              {article.attributes.title}
+            </div>
+          </a>
+        </Link>
+        <div className={classes.Article__NotImage_belowTitle}>
           <div>
-            {new Date(article.attributes.dateline).toLocaleDateString(
-              "en-US",
-              dateOptions
-            )}
+            <svg>
+              <use xlinkHref="/images/sprite.svg#icon-user"></use>
+            </svg>
+            <div>{article.attributes.author}</div>
+          </div>
+          <div>
+            <svg>
+              <use xlinkHref="/images/sprite.svg#icon-calendar"></use>
+            </svg>
+            <div>
+              {new Date(article.attributes.dateline).toLocaleDateString(
+                "en-US",
+                dateOptions
+              )}
+            </div>
           </div>
         </div>
-      </div>
-      <div className={classes.Article__NotImage_excerpt}>
-        {article.attributes.body
-          .replace(/<br>/g, " ")
-          .replace(/<[^>]+>/g, "")
-          .split(" ")
-          .splice(0, 16)
-          .join(" ")}
-        ...
+        <div className={classes.Article__NotImage_excerpt}>
+          {article.attributes.body
+            .replace(/<br>/g, " ")
+            .replace(/<[^>]+>/g, "")
+            .split(" ")
+            .splice(0, 16)
+            .join(" ")}
+          ...
+        </div>
       </div>
     </div>
   </div>
