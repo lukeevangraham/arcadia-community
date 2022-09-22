6d00218d662ea33e7ac58eeb922a528ed1b5124f
Luke Graham <luke.evan.graham@gmail.com>
2022-09-22 12:37:14 -0700

    working on staff images

--- components/About/StaffMember/StaffMember.js
+++ components/About/StaffMember/StaffMember.js
@@ -4,20 +4,27 @@ import classes from "./StaffMember.module.scss";
 
 const StaffMember = ({ person }) => (
   <div className={classes.Person}>
-    <div className={classes.Person__image}>
-      <Image
-        src={person.attributes.photo.data.attributes.url}
-        alt={person.attributes.photo.data.attributes.alternativeText}
-        layout="fill"
-        objectFit="cover"
-      ></Image>
+    <div className={classes.Person__ImageWrapper}>
+      <div className={classes.Person__ImageWrapper__image}>
+        <Image
+          src={person.attributes.photo.data.attributes.url}
+          alt={person.attributes.photo.data.attributes.alternativeText}
+          layout="fill"
+          objectFit="cover"
+        ></Image>
+      </div>
+      <div className={classes.Person__ImageWrapper__overlay}>
+        <p>{person.attributes.shortBio}</p>
+      </div>
     </div>
     <div className={classes.Person__text}>
       <div className={classes.Person__text_name}>
         {person.attributes.title ? `${person.attributes.title} ` : null}
         {`${person.attributes.firstName} ${person.attributes.lastName}`}
       </div>
-      <div  className={classes.Person__text_title}>{person.attributes.jobTitle}</div>
+      <div className={classes.Person__text_title}>
+        {person.attributes.jobTitle}
+      </div>
     </div>
   </div>
 );
