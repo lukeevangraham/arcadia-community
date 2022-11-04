d0a424978aad17ded03b44c18888fa2a0ede66ee
Luke Graham <luke.evan.graham@gmail.com>
2022-11-04 08:59:48 -0700

    adding a contition to seo share image

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -1,35 +1,42 @@
 import { NextSeo } from "next-seo";
 
 const SEO = ({ metaData }) => (
-  <NextSeo
-    title={metaData.metaTitle}
-    description={metaData.metaDescription}
-    openGraph={
-      metaData && metaData.shareImage && metaData.shareImage.data
-        ? {
-            title: metaData.metaTitle,
-            description: metaData.metaDescription,
-            images: [
-              {
-                url: metaData.shareImage.data.attributes.url,
-                width: metaData.shareImage.data.attributes.width,
-                height: metaData.shareImage.data.attributes.height,
-                alt: metaData.shareImage.data.attributes.alternativeText,
-              },
-              {
-                url: metaData.shareImage.data.attributes.formats.thumbnail.url,
-                width:
-                  metaData.shareImage.data.attributes.formats.thumbnail.width,
-                height:
-                  metaData.shareImage.data.attributes.formats.thumbnail.height,
-                alt: metaData.shareImage.data.attributes.formats.thumbnail
-                  .alternativeText,
-              },
-            ],
-          }
-        : { title: metaData.metaTitle, description: metaData.metaDescription }
-    }
-  />
+  <>
+    <NextSeo
+      title={metaData.metaTitle}
+      description={metaData.metaDescription}
+      openGraph={
+        metaData &&
+        metaData.shareImage &&
+        metaData.shareImage.data &&
+        metaData.shareImage.data.formats
+          ? {
+              title: metaData.metaTitle,
+              description: metaData.metaDescription,
+              images: [
+                {
+                  url: metaData.shareImage.data.attributes.url,
+                  width: metaData.shareImage.data.attributes.width,
+                  height: metaData.shareImage.data.attributes.height,
+                  alt: metaData.shareImage.data.attributes.alternativeText,
+                },
+                {
+                  url: metaData.shareImage.data.attributes.formats.thumbnail
+                    .url,
+                  width:
+                    metaData.shareImage.data.attributes.formats.thumbnail.width,
+                  height:
+                    metaData.shareImage.data.attributes.formats.thumbnail
+                      .height,
+                  alt: metaData.shareImage.data.attributes.formats.thumbnail
+                    .alternativeText,
+                },
+              ],
+            }
+          : { title: metaData.metaTitle, description: metaData.metaDescription }
+      }
+    />
+  </>
 );
 
 export default SEO;
